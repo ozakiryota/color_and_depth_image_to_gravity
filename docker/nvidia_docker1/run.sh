ade1686fb632d2345dbecc9ae6625c030d7edcfd
ozakiryota <kb22056@gmail.com>
2020-11-13 15:21:38 +0900

    first commit

---
+++ docker/nvidia_docker1/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+image_name="color_and_depth_image_to_gravity"
+tag_name="nvidia_docker1"
+root_path=$(pwd)
+
+xhost +
+nvidia-docker run -it --rm \
+	--env="DISPLAY" \
+	--env="QT_X11_NO_MITSHM=1" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	-v $root_path/../../../dataset_image_to_gravity:/home/$image_name/../dataset_image_to_gravity \
+	-v $root_path/../../weights:/home/$image_name/weights \
+	-v $root_path/../../graph:/home/$image_name/graph \
+	-v $root_path/../../logs:/home/$image_name/logs \
+	$image_name:$tag_name
